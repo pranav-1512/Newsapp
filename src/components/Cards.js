03315581caa74959e4f1c4e18d29e10c757170b3
pranav-1512 <135528405+pranav-1512@users.noreply.github.com>
2024-07-16 17:22:58 +0530

    Update Cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -8,11 +8,11 @@ export class Cards extends Component {
                 <div className="card" style= {{width: "18rem"}}>
                 <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex: '1'}}>{author?author:'Unknown'}
                         </span>
-                    <img src={urlToImage} className="card-img-top" alt="Image"/>
+                    <img src={urlToImage} className="card-img-top" />
                         <div className="card-body">
                             <h5 className="card-title">{title}</h5>
                             <p className="card-text">{description}</p>
-                            <a href={url} target='_blank' className="btn btn-primary">Read More</a>
+                            <a href={url} target='_blank' rel="noreferrer" className="btn btn-primary">Read More</a>
                         </div>
                         <p className='mx-3'><strong>Published At: </strong>{publishedAt.slice(0,10)}</p>
                 </div>
